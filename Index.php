54af89233205ac0cfdaceb57643b27ccada3d9fd
wellraul <well.raulG@gmail.com>
2017-06-04 18:54:55 -0300

    first commit

---
+++ Index.php
@@ -0,0 +1,29 @@
+<?php
+spl_autoload_register(function($nomeDaClasse) {
+    require_once($nomeDaClasse.".php");
+});
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Desing Patterns</title>
+  </head>
+  <body>
+
+
+    <h1>Regra de descontos</h1>
+<?php
+    $orcamento = new Orcamento(1500);
+    $realiza = new CalculadoraDeDescontos();
+    $orcamento->addItem(new Item("ferro", 250));
+    $orcamento->addItem(new Item("tijolo", 250));
+    $orcamento->addItem(new Item("ferro", 250));
+    $orcamento->addItem(new Item("cimento", 250));
+    $orcamento->addItem(new Item("areia", 250));
+    $orcamento->addItem(new Item("Azulejo", 250));
+
+    echo $realiza->realizaDesconto($orcamento);
+ ?>
+  </body>
+</html>
